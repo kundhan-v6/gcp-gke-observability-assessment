9d00b9b4bb945a5eccc751dde77bf0b1c9c94c3a
kundhan-v6 <kundhan.v6@gmail.com>
2026-09-25 04:57:24 -0400

    Manage application Secret Manager version

--- terraform/secret-manager.tf
+++ terraform/secret-manager.tf
@@ -19,3 +19,14 @@ resource "google_secret_manager_secret_iam_member" "app_runtime_secret_access" {
 
   member = "serviceAccount:gke-app-runtime@gcp-gke-assessment.iam.gserviceaccount.com"
 }
+
+resource "google_secret_manager_secret_version" "app_runtime_secret_version" {
+  secret                 = google_secret_manager_secret.app_runtime_secret.id
+  secret_data_wo         = var.app_demo_secret
+  secret_data_wo_version = var.app_demo_secret_wo_version
+}
+
+output "app_runtime_secret_resource" {
+  description = "Secret Manager resource mounted by GKE workloads"
+  value       = google_secret_manager_secret.app_runtime_secret.id
+}

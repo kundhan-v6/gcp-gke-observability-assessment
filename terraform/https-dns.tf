48bc98122d3e980678556143290fef3e205232b7
kundhan-v6 <kundhan.v6@gmail.com>
2026-09-25 04:58:32 -0400

    Bootstrap DNS IAM before adopting DNS record

--- terraform/https-dns.tf
+++ terraform/https-dns.tf
@@ -1,21 +1,3 @@
-data "google_dns_managed_zone" "photography" {
-  project = var.project_id
-  name    = "kundhanphotography-com"
-}
-
-resource "google_dns_record_set" "gke_endpoint" {
-  project      = var.project_id
-  managed_zone = data.google_dns_managed_zone.photography.name
-  name         = "gke.kundhanphotography.com."
-  type         = "A"
-  ttl          = 300
-  rrdatas      = [google_compute_global_address.mci_global_ip.address]
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
 resource "google_compute_managed_ssl_certificate" "gke_endpoint" {
   project = var.project_id
   name    = "gke-kundhanphotography-cert"
@@ -27,11 +9,6 @@ resource "google_compute_managed_ssl_certificate" "gke_endpoint" {
   lifecycle {
     prevent_destroy = true
   }
-}
-
-import {
-  to = google_dns_record_set.gke_endpoint
-  id = "projects/gcp-gke-assessment/managedZones/kundhanphotography-com/rrsets/gke.kundhanphotography.com./A"
 }
 
 import {
